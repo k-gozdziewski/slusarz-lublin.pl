be9f26737d78b8a19646a7ad28b072aa1481c626
k-gozdziewski <41209405+k-gozdziewski@users.noreply.github.com>
2024-04-07 18:39:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Ślusarz Lublin - awaryjne otwieranie drzwi</title>
+        <meta name="description" content="Ślusarz Lublin - awaryjne otwieranie drzwi, wymiana zamków, usługi ślusarskie."/>
+        <link href="main.css" rel="stylesheet" type="text/css">
+        <!-- Bootstrap -->
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container" id="tło">
+            <div class="row" id="topsection">
+                <div class="col-xs-12 text-center">
+                    <div id="logo">
+                        <img class="inline" src="logo.png" width=100px height=100px> <h1 class="inline" id="logoTitle">Ślusarz Lublin</h1>
+                    </div>
+                    <br>
+                    <h1 id="podtytul"> Awaryjne otwieranie drzwi</h1>
+                    <a class="btn btn-success btn-xlg" href="tel:662921442" style="padding:20px;">
+                        Zapraszam do kontaktu <span class="glyphicon glyphicon-earphone" style="padding-left: 10px; padding-right: 10px"></span>662-921-442
+                    </a>
+                    <br><br>
+                </div>
+            </div>
+            <div id="opis">
+                <div class="row rowSpace">
+                    <div class="col-md-4">
+                        <img src="locksmith.jpg" class="img-responsive center-block img-circle" >
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="white font-weight-bold">Od lat prowadzę firmę ślusarską co pozwoliło mi zdobyć duże doświadczenie. Częste dojazdy do klienta pozwoliły mi zetknąć się z różnymi nietypowymi przypadkami zablokowanych i zamkniętych drzwi. Dokładam wszelkich starań aby oferować usługi ślusarskie na najwyższym poziomie.</h3>
+                    </div>
+                </div> 
+                <div class="row rowSpace">
+                    <div class="col-md-4 col-md-push-8">
+                        <img src="tools.png" class="img-responsive center-block img-circle" >
+                    </div>
+                    <div class="col-md-8 col-md-pull-4">
+                        <h3 class="white font-weight-bold">Zajmuję się zarówno awaryjnym otwieraniem zaciętych drzwi jak i wykonywaniem w swoim warsztacie a później montażem u klienta: ogrodzeń panelowych, z siatki oraz innych elementów metalowych.</h3>
+                    </div>
+                </div> 
+                <div class="row rowSpace">
+                    <div class="col-md-4">
+                        <img src="24%20na%207.png" class="img-responsive center-block img-circle" >
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="white font-weight-bold"> Oferuję usługi 24 godziny na dobę, 7 dni w tygodniu.</h3>
+                    </div>
+                </div> 
+            </div>
+            <div class="row text-center">
+                <div id="kontakt">
+                    <h1> Zapraszam do kontaktu!</h1>
+                    <a href="tel:+48662921442" ><h1> 662-921-442 </h1></a>
+                </div>
+            </div>
+            <footer>&copy; Made by Kamil Goździewski</footer>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
